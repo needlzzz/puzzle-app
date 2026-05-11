d3c05509fcc21997ff6b768d5b825081380cf830
Marc Kaelin <marc.kaelin@swisscom.com>
2026-05-11 17:11:46 +0200

    fix(ios): reduce piece tray long press duration for faster interaction
    - Decrease minimumPressDuration from 0.09 to 0.05 seconds in PieceTrayUIView
    - Enables quicker piece pickup and drag initiation for improved user responsiveness
    - Aligns with goal of streamlining puzzle interaction speed

--- ios/KaleysPuzzle/PieceTrayView.swift
+++ ios/KaleysPuzzle/PieceTrayView.swift
@@ -90,7 +90,7 @@ class PieceTrayUIView: UIView, UIGestureRecognizerDelegate {
 
         // Add a long press gesture on the scroll view to pick up pieces
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handlePieceDrag(_:)))
-        longPress.minimumPressDuration = 0.09
+        longPress.minimumPressDuration = 0.05
         longPress.delegate = self
         scrollView.addGestureRecognizer(longPress)
 
